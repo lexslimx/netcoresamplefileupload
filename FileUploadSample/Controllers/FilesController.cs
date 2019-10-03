8763125398011062f80fbc998b478a065e6960c2
Alex Maina <alex.km@live.com>
2019-10-03 10:13:39 +0400

    initial commint

---
+++ FileUploadSample/Controllers/FilesController.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using FileUploadSample.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FileUploadSample.Controllers
+{
+    public class FilesController : Controller
+    {
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(FileDto fileDto, IFormFile fileUpload)
+        {
+            try
+            {
+                // TODO: Add insert logic here
+
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
---
+++ FileUploadSample/Models/FileDto.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FileUploadSample.Models
+{
+    public class FileDto
+    {       
+        public string FileName { get; set; }
+        public IFormFile ProfilePic { get; set; }
+    }
+}
